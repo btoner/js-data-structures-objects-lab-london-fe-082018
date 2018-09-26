96e73377069b2bf95f07c443bc69cde1d26cacae
btoner <ben.toner@wework.com>
2018-09-26 19:34:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ function updateDriverWithKeyAndValue(driver, key, value){
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
-  return driver = Object.assign({name: 'Sam', address: '11 Broadway'});
+  return driver = Object.assign({name: 'Sam', address: '12 Broadway' });
 }
