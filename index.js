4aa82a286583a44b5915b02aacebe69b4403c525
btoner <ben.toner@wework.com>
2018-09-26 19:40:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
 }
 
 function deleteFromDriverByKey(driver, key){
-  
+  delete 
 }
 
 function destructivelyDeleteFromDriverByKey(){
